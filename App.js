6c0300abc260adb8095766bdaf6d384869d85549
konapuramsureshbabu <konapuramsureshbabu@gmail.com>
2023-01-27 13:06:17 +0530

    first commit

---
+++ App.js
@@ -0,0 +1,17 @@
+import pizzaHut from './components/pizzaHut';
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <div className="header">
+        <h1 className="use">Suresh</h1>
+        <pizzaHut />
+      </div>
+      
+    
+    </>
+  );
+}
+
+export default App;
---
+++ components/project.py
@@ -0,0 +1,6 @@
+a='sureshbabureddy'
+b='rajeshbabu'
+if len(a)>len(b):
+    print('a len is more')
+else:
+    print('b len is more')
